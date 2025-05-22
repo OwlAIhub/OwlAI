f5e79a7e1b669d09206e30b1222ed09d64bccef4
ahsnify <ahsanshazi786yo@gmail.com>
2025-05-22 22:22:54 +0530

    Added new files

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'media',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'nc-base': '#0D1B2A',
+        'nc-primary': '#009688',
+        'nc-accent': '#FFC107',
+      },
+    },
+  },
+  plugins: [],
+};
