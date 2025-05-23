8c01bba3623ca283d8c38e4bc888be0be9e5478b
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-05-23 15:51:06 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
 import Header from "./Components/Header";
 import MainContent from "./Components/MainContent";
